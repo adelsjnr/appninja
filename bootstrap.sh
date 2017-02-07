dc07c3a1818eba6c37f82dbc89a66f5b596aa10e
Adelson Oliveira <adelson@vizir.com.br>
2017-02-07 03:32:06 -0200

    Some fix and improvments

---
+++ bootstrap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Install some basic packages
+apt-get update
+apt-get install -y vim curl git
+
+# Install ruby version 2.1.1 with rvm
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+\curl -sSL https://get.rvm.io | bash -s stable
+source /etc/profile.d/rvm.sh
+rvm install 2.1.1
+
+# Install chef-solo
+curl -L https://www.opscode.com/chef/install.sh | bash
+chef-solo -v
+
